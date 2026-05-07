c5e83b38a6b1ba9452334e46513b0813e2f7e51b
Youssef-ali-1 <youssefaali31@gmail.com>
2026-05-07 23:28:33 +0300

    login.js

--- js/login.js
+++ js/login.js
@@ -1,22 +1,40 @@
 
 const loginForm = document.getElementById('login-form');
-if (loginForm) {
-    loginForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const email = document.getElementById('login-email').value;
-        const password = document.getElementById('login-password').value;
+const emailInput = document.getElementById('login-email');
+const passwordInput = document.getElementById('login-password'); 
+const emailError = document.getElementById('email-error'); 
+const passwordError = document.getElementById('password-error'); 
+if (loginForm) { loginForm.addEventListener('submit', (e) => {
+    e.preventDefault(); let isValid = true;
+    if (emailInput.value.trim() === '') {
+        emailError.textContent = 'Email address is required.';
+        emailInput.classList.add('input-error-style'); isValid = false;
+    }
+    else {
+        emailError.textContent = ''; emailInput.classList.remove('input-error-style');
+    }
+    if (passwordInput.value.trim() === '') {
+        passwordError.textContent = 'Password is required.'; 
+        passwordInput.classList.add('input-error-style'); isValid = false;
+    } 
+    else {
+        passwordError.textContent = '';
+        passwordInput.classList.remove('input-error-style'); 
+    }
+    if (isValid === true) {
         const savedUserString = localStorage.getItem('projectUser');
         if (savedUserString) {
-            const savedUser = JSON.parse(savedUserString);
-            if (email === savedUser.userEmail && password === savedUser.userPassword) {
-                alert('Welcome back, ' + savedUser.userName + '!');
-                localStorage.setItem('isLoggedIn', 'true');
-                window.location.href = './index.html'; /* team  */
-            } else {
-                alert('Invalid email or password. Please try again.');
+            const savedUser = JSON.parse(savedUserString); 
+            if (emailInput.value === savedUser.userEmail && passwordInput.value === savedUser.userPassword) {
+                alert('Welcome back, ' + savedUser.userName + '!'); 
+                localStorage.setItem('isLoggedIn', 'true'); window.location.href = '../main.html';
+                passwordError.textContent = 'Invalid email or password.';
+                passwordInput.classList.add('input-error-style'); 
             }
-        } else {
-            alert('No account found. Please sign up first.');
         }
-    });
-}
+        else {
+            alert('No account found. Please sign up first.'); 
+        }
+    }
+});
+               }
